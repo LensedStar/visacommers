b5c8ba1fdec4cda6b6d70d5e41d857a55d7b6edf
LensedStar <daniil.letkovskyisp@gmail.com>
2023-10-03 11:56:13 +0200

    Create for who block

--- src/Components/MainPage/WhoBlockMain/WhoBlock.jsx
+++ src/Components/MainPage/WhoBlockMain/WhoBlock.jsx
@@ -6,6 +6,8 @@ import { useTranslation } from "react-i18next";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import {motion , AnimatePresence} from "framer-motion";
+
 import { faBriefcase,faGraduationCap,faChevronLeft,faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function WhoBlock() {
@@ -18,45 +20,65 @@ const handleMinus = () => {
     slide === 1 ? setSlide(0) : setSlide(1);
 }
 
-const handleSlide = () => {
-    if (slide === 0) {
-        return (
-            <aside className="variant variant1">
+    return(
+       <div className="whoBlock">
+
+           <h2 className="whoTitle">{t("main.forWho")}</h2>
+           <motion.article
+               className="variants"
+                initial={{opacity:0,x:-100}}
+                whileInView={{opacity:1,x:0}}
+               transition={{duration:0.5,}}
+           >
+               <AnimatePresence>
+               {slide === 0 ? <motion.aside
+                   className="variant variant1"
+                   key="1"
+                   initial={{x:-100}}
+                   animate={{opacity:1,x:0}}
+                   exit={{opacity:0,x:100,display:"none"}}
+                   transition={{
+                       duration: 0.5,
+                   }}
+
+               >
                    <span className="icon">
                         <FontAwesomeIcon icon={faBriefcase} size="2xl" style={{color: "#343742",}}/>
                    </span>
-                <article className="variantDesc variantDesc1">
-                    <h4>{t("mock.title")}</h4>
-                    <p>{t("mock.description-short")}</p>
-                </article>
-            </aside>
-        )
-    } else if (slide === 1) {
-        return (
-            <aside className="variant variant2">
+                   <article className="variantDesc variantDesc1">
+                       <h4>{t("mock.title")}</h4>
+                       <p>{t("mock.description-short")}</p>
+                   </article>
+               </motion.aside>
+                   :
+                   <motion.aside className="variant variant2"
+                                 key="2"
+                                 initial={{x:200,}}
+                                 animate={{opacity:1,x:0}}
+                                 exit={{opacity:0,display:"none"}}
+                                 transition={{
+                                     duration: 0.5,
+                                 }}
+                   >
                    <span className="icon">
                        <FontAwesomeIcon icon={faGraduationCap} size="2xl" style={{color: "#343742",}}/>
                    </span>
-                <article className="variantDesc variantDesc2">
-                    <h4>{t("mock.title")}</h4>
-                    <p>{t("mock.description-short2")}</p>
-                </article>
-            </aside>
-        )
-    }
-}
-    return(
-       <div className="whoBlock">
-           <h2 className="whoTitle">{t("main.forWho")}</h2>
-           <article className="variants">
-               {
-                     handleSlide()
+                       <article className="variantDesc variantDesc2">
+                           <h4>{t("mock.title")}</h4>
+                           <p>{t("mock.description-short2")}</p>
+                       </article>
+                   </motion.aside>
                }
+                </AnimatePresence>
                <div className="variantsBtns">
-                   <FontAwesomeIcon icon={faChevronLeft} size="xl" onClick={()=>handleMinus()}/>
-                   <FontAwesomeIcon icon={faChevronRight} size="xl" onClick={()=>handlePlus()}/>
+                   <span className="arrLeft">
+                        {slide===1 && <FontAwesomeIcon icon={faChevronLeft} size="xl" onClick={()=>handleMinus()}/>}
+                   </span>
+                   <span className="arrRight">
+                        {slide===0 && <FontAwesomeIcon icon={faChevronRight} size="xl" onClick={()=>handlePlus()}/>}
+                   </span>
                </div>
-           </article>
+           </motion.article>
        </div>
     )
 }
