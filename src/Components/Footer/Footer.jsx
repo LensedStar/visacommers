bf0f2fb5fc93b0371049b76b7d0c67710b37eba7
LensedStar <daniil.letkovskyisp@gmail.com>
2023-10-09 15:22:34 +0200

    Hide reCpatcha

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,6 +5,9 @@ export default function Footer() {
     return(
         <footer className="footer">
                 <p>All rights reserved © 2023</p>
+            <p>This site is protected by reCAPTCHA and the Google</p>
+            <a href="https://policies.google.com/privacy">Privacy Policy</a> and
+            <a href="https://policies.google.com/terms">Terms of Service</a> apply.
         </footer>
     )
 }
