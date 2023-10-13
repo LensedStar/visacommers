b663afb167e27a3908f9c6b12c86642d29cb6ede
LensedStar <daniil.letkovskyisp@gmail.com>
2023-10-13 10:42:40 +0200

    Delete useless pages

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export default function About() {
     return(
         <div>
-            <h1>О нас</h1>
-            <p>Мы - контора пидарасов , которые создали этот сайт.</p>
+            <h1>Nothing here</h1>
         </div>
     )
 }
--- src/Components/Something.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Something() {
-    return(
-        <div>
-            <h1>Игорь ты гей!</h1>
-        </div>
-    )
-}
